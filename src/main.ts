7a895bca2476e6850a6fc01e3e1d7e89ee8cfe0f
Paulo Serrano <pvsserrano@gmail.com>
2024-05-09 19:07:42 -0300

    postinstall script

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
